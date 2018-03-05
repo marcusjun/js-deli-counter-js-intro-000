4bfa2dfb8059ba221a5386ed330afaa591666723
Marcus Jun <mjun@calstate.edu>
2018-03-05 00:53:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var katzDeli=[]
 function takeANumber(katzDeliLine,name){
   katzDeli.push(name)
 
-  return `Welcome, ${name}. You are ${katzDeli[name]+1} in line.`
+  return `Welcome, ${name}. You are ${katzDeli[name]} in line.`
 }
