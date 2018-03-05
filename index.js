4dfa988eaddfdd88731b7ed503622cc1ef61c43a
Marcus Jun <mjun@calstate.edu>
2018-03-05 01:05:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ function takeANumber(katzDeliLine,name){
 function nowServing(katzDeliLine){
   var n=0
 
+  if (katzDeliLine.length===0){
+    return "The line is currently empty"
+  }
+
   return "The line is currently: "
 
   while(n<katzDeliLine.length){
