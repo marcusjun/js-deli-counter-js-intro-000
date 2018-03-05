0d4c8acb8b4579e3abac80752bd53c433b9e893b
Marcus Jun <mjun@calstate.edu>
2018-03-05 01:06:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nowServing(katzDeliLine){
   var n=0
 
   if (katzDeliLine.length===0){
-    return "The line is currently empty"
+    return "There is nobody waiting to be served!"
   }
 
   else {
